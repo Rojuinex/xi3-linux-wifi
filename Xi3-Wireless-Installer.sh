3fa7fff2aa716a507233447b1d5e5af7bc3e13ea
Caleb Bartholomew <rojuinex@gmail.com>
2013-04-19 14:55:22 -0500

    Polishing up install script

--- Xi3-Wireless-Installer.sh
+++ Xi3-Wireless-Installer.sh
@@ -1,3 +1,4 @@
+rm -rf /tmp/worker.sh
 echo 'echo "This script needs administrator privileges.  The default password is xi3password"' > /tmp/worker.sh
 echo 'sudo rm -rf xi3-temp-wlinstall' >> /tmp/worker.sh
 echo 'mkdir xi3-temp-wlinstall' >> /tmp/worker.sh
@@ -13,9 +14,11 @@ echo 'echo -e "\n" >> instructions-pt1' >> /tmp/worker.sh
 echo 'echo -e "\n" >> instructions-pt1' >> /tmp/worker.sh
 echo 'echo "You may press any key in the terminal to continue installation." >> instructions-pt1' >> /tmp/worker.sh
 echo 'kwrite instructions-pt1 </dev/null &>/dev/null &' >> /tmp/worker.sh
+echo 'dolphin ~/xi3-temp-wlinstall/docs' >> /tmp/worker.sh
 echo 'echo "Press any key to continue..."' >> /tmp/worker.sh
 echo 'read -n 1 -s' >> /tmp/worker.sh
 echo 'popd' >> /tmp/worker.sh
+
 echo 'pushd .' >> /tmp/worker.sh
 echo 'sudo zypper removerepo http://www2.ati.com/suse/11.4' >> /tmp/worker.sh
 echo 'sudo zypper install kernel-source=2.6.37.6-0.7' >> /tmp/worker.sh
@@ -27,9 +30,10 @@ echo 'sudo zypper install gcc' >> /tmp/worker.sh
 echo 'sudo chmod 755 install.sh' >> /tmp/worker.sh
 echo 'sudo ./install.sh' >> /tmp/worker.sh
 echo 'popd' >> /tmp/worker.sh
+
 echo 'pushd docs' >> /tmp/worker.sh
 echo 'echo "Now open YaST (Under Start > Computer > YaST) and enter the password (Default is xi3password)." > instructions-pt2' >> /tmp/worker.sh
-echo 'echo "In YaST, select Network Devices from the left side, and then Network Settings" > instructions-pt2' >> /tmp/worker.sh
+echo 'echo "In YaST, select Network Devices from the left side, and then Network Settings" >> instructions-pt2' >> /tmp/worker.sh
 echo 'echo "In the new window, click the Global Options tab and under Network Setup Method select User Controlled with NetworkManager." >> instructions-pt2' >> /tmp/worker.sh
 echo 'echo "Select OK." >> instructions-pt2' >> /tmp/worker.sh
 echo 'echo "" >> instructions-pt2' >> /tmp/worker.sh
@@ -38,12 +42,14 @@ echo 'kwrite instructions-pt2 </dev/null &>/dev/null &' >> /tmp/worker.sh
 echo 'echo "Press any key to continue..."' >> /tmp/worker.sh
 echo 'read -n 1 -s' >> /tmp/worker.sh
 echo 'knetworkmanager </dev/null &>/dev/null &' >> /tmp/worker.sh
-echo 'echo "Please unplug the Ethernet cable at this time and connect to a wireless network from the icon in the tray. It should look like an Ethernet port." > instructions-pt3' >> /tmp/worker.sh
+echo 'echo -e "Please unplug the Ethernet cable at this time and connect to a wireless network from the Ethernet icon in the tray.\n\nYou can now press any key to close the installer." > instructions-pt3' >> /tmp/worker.sh
 echo 'kwrite instructions-pt3 </dev/null &>/dev/null &' >> /tmp/worker.sh
 echo 'popd' >> /tmp/worker.sh
+echo 'echo -e "Instillation finished\!\n\nPress any key to exit."' >> /tmp/worker.sh
+echo 'read -n 1 -s' >> /tmp/worker.sh
 
 chmod 755 /tmp/worker.sh
 
-konsole -noclose -e bash -e /tmp/worker.sh
+konsole -e bash -e /tmp/worker.sh
 
 
